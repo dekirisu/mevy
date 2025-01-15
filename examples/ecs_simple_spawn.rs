fdec71574abef161b8a2af64eb8fd1d8e8f8ea5a
Dekirisu <dekirisu@gmail.com>
2025-01-15 15:33:58 +0000

    🐁 use core changes in an example

--- examples/ecs_simple_spawn.rs
+++ examples/ecs_simple_spawn.rs
@@ -10,25 +10,26 @@ pub fn main() {
 }
 
 fn startup(mut world:Commands){
-    spawn!{Camera2d::default()}
+    spawn!{Camera2d}
     spawn!{
         BackgroundColor(#ff0000);
         BorderColor(#00ffff);
-        Node{ width:80px, height:80px, margin:[>16px], ..default()};
+        Node{ width:80px, height:80px, margin:[>16px], !};
         .observe(destroy_on::<Click>(e1));
         .observe(destroy_on::<Click>(named_child));
         [ // auto-called 'e1'
-            Node{ width:20%, height:20%, ..default() };
+            Node{ width:20%, height:20%, !};
             BackgroundColor(#00ff00);
         ]
         [
-            Node{ width:20%, height:20%, ..default() };
+            Node{ width:20%, height:20%, !};
             BackgroundColor(#00aaff);
             .observe(destroy_self_on::<Click>);
         ]
         [named_child][
-            Node{ width:20%, height:20%, ..default() };
+            Node{ width:20%, height:20%, !};
             BackgroundColor(#00ffaa);
+            Visibility!;
         ]
     }
 }
