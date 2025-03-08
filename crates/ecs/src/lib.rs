b063c5225d39ebc8b341d5b235e9c765efc48c6e
Dekirisu <dekirisu@gmail.com>
2025-03-08 19:44:45 +0000

    🐤 ecs: add modify! macro
    ```rust
    // Required variables
    // world: Commands (mutable)
    // me:    Entity
    modify!{
        Node!;
    }
    
    // OR using *:
    // this: EntityCommands (or similar)
    modify!{*
        Node!;
    }
    ```

--- crates/ecs/src/lib.rs
+++ crates/ecs/src/lib.rs
@@ -1,7 +1,19 @@
 use mevy_ecs_syntax::*;
 use proc_macro::TokenStream as CompilerTokens;
+use deki::*;
 
 #[proc_macro]
 pub fn spawn(stream:CompilerTokens) -> CompilerTokens {
     spawn_syntax(stream.into()).into()
+}
+
+#[proc_macro]
+pub fn modify(stream:CompilerTokens) -> CompilerTokens {
+    let stream: TokenStream = stream.into();
+    let mut iter = stream.peek_iter();
+    let pre = match iter.peek_punct() {
+        '*' => {iter.next();qt!(*this;)},
+        _ => qt!(&me;)
+    };
+    spawn_syntax(qt!(#pre #(#iter)*)).into()
 }
